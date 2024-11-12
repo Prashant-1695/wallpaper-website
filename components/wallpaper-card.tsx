ada987cb135788dceb9754036b2b1a97601d8253
shubhamxdd <ssisodia128@gmail.com>
2024-11-12 10:45:30 +0530

    remove bookmark icon

--- components/wallpaper-card.tsx
+++ components/wallpaper-card.tsx
@@ -77,23 +77,6 @@ const WallpaperCard = ({
     <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
     </svg>
     </button>
-    <button className="p-2 bg-white/20 hover:bg-white/30 rounded-lg backdrop-blur-sm transition-colors">
-    {/* Bookmark Icon */}
-    <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="text-white"
-    >
-    <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z" />
-    </svg>
-    </button>
     </div>
     </div>
     </div>
