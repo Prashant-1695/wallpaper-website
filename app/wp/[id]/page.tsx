8d59b0b769e63399b61e68b97804ef315e11167a
Prashant-1695 <prashant33968@gmail.com>
2024-11-25 09:25:02 +0530

    Add Navigation Button
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>

--- app/wp/[id]/page.tsx
+++ app/wp/[id]/page.tsx
@@ -1,7 +1,10 @@
-// app/wp/[id]/page.tsx
-import { getCloudinaryImageById } from "@/config/cloudinary";
+import {
+  getCloudinaryImages,
+  getCloudinaryImageById,
+} from "@/config/cloudinary";
 import { notFound } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function WallpaperPage({
   params,
@@ -11,17 +14,26 @@ export default async function WallpaperPage({
   const { id } = await params;
 
   const wall = await getCloudinaryImageById(id);
+  const images = await getCloudinaryImages(); // Fetch all images
 
   if (!wall) {
     notFound();
   }
+
+  // Find the current image index
+  const currentIndex = images.findIndex(
+    (image) => image.public_id === wall.public_id
+  );
+  const previousImage = currentIndex > 0 ? images[currentIndex - 1] : null;
+  const nextImage =
+    currentIndex < images.length - 1 ? images[currentIndex + 1] : null;
 
   // TODO: Hardcoding for now! to be fetched from database
   const likes = Math.ceil(Math.random() * 10);
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="max-w-4xl mx-auto">
+      <div className="max-w-4xl mx-auto relative">
         <Image
           src={wall.secure_url}
           alt="Wallpaper"
@@ -46,6 +58,26 @@ export default async function WallpaperPage({
           >
             Download Wallpaper
           </a>
+        </div>
+
+        {/* Navigation Buttons */}
+        <div className="absolute inset-0 flex items-center justify-center">
+          {previousImage && (
+            <Link
+              href={`/wp/${previousImage.public_id}`}
+              className="absolute top-1/4 sm:top-1/3 left-4 transform -translate-y-1/2 text-4xl text-white bg-gray-800 bg-opacity-50 hover:bg-opacity-75 rounded-full w-10 h-10 flex items-center justify-center transition-colors duration-200"
+            >
+              &lt;
+            </Link>
+          )}
+          {nextImage && (
+            <Link
+              href={`/wp/${nextImage.public_id}`}
+              className="absolute top-1/4 sm:top-1/3 right-4 transform -translate-y-1/2 text-4xl text-white bg-gray-800 bg-opacity-50 hover:bg-opacity-75 rounded-full w-10 h-10 flex items-center justify-center transition-colors duration-200"
+            >
+              &gt;
+            </Link>
+          )}
         </div>
       </div>
     </div>
